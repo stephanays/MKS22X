0bcabed2a02072f80e18c1f791ed4c99c32a782b
stephanays <stephaniezheng1@gmail.com>
2017-02-06 17:08:00 -0500

    Square Root

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+public class Recursion{
+    
+    public static String name(){
+	return "Zheng,Stephanie";
+    }
+    
+    public static double sqrt(double n){
+	if(n<0){
+	    throw new IllegalArgumentException();
+	}
+	return helper(n,1);
+    }
+
+    public static double helper(double n, double guess){
+	if((Math.abs(n - (guess * guess)))<0.0000000001){
+	    return guess;
+	}else{
+	    return helper(n,(((n/guess)+guess)/2));
+	}
+    }
+
+    public static void main(String[]args){
+	System.out.println(sqrt(2));
+    }
+}
