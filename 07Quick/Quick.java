987583351a137de986ef4edf599fd8896ff1f96d
stephanays <stephaniezheng1@gmail.com>
2017-03-24 10:16:04 -0400

    finally got em ah yes

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,19 +1,26 @@
 public class Quick{
     
     public static int part(int[] data, int start, int end){
-	int pivot = (int) (Math.random() * (end - start));
+	int pivot = (int) (Math.random() * (end - start)) + start;
 	int value = data[pivot];
-	int spot = 0;
-	for(int x=0; x<data.length; x++){
-	    if(data[x]<value){
-		swap(data, x, spot);
-		spot++;
+	int i = start;
+	int lt = start;
+	int gt = end;
+	swap(data, pivot, i);
+	while(i<=gt){
+	    if(data[i] == value){
+		i++;
+	    }else if(data[i] < value){
+		swap(data, i, lt);
+		i++;
+		lt++;
+	    }else{
+		swap(data, i, gt);
+		gt--;
 	    }
-
 	}
-	swap(data, pivot,spot);
 	System.out.println(value);
-	return spot;
+	return lt;  
     }
 
     public static void swap(int[] data, int x, int y){
@@ -43,7 +50,7 @@ public class Quick{
     }
 
     public static void main(String[]args){
-	int[]test={3,7,8,4,2,1,5,9,0,6};
+	int[]test={3,2,7,8,4,2,1,5,9,0,6,4,4,2,6};
 	System.out.println(part(test,0,test.length-1));
 	System.out.println(toString(test));
     }
