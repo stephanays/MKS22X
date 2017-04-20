812bfbd59da4d193ecdaf15b6abe21fc83dba0c4
stephanays <stephaniezheng1@gmail.com>
2017-04-20 00:19:44 -0400

    done c:

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,70 +1,189 @@
-public class MyLinkedList{
-    private LNode start;
-    private LNode end;
+import java.util.*;
+public class MyLinkedList implements Iterable<Integer>{
+    private LNode head;
+    private LNode tail;
     private int size;
 
-    public MyLinkedList(){
-	size=0;
+    private class LNode{
+	private int value;
+	private LNode next;
+	private LNode prev;
+
+	public LNode(int quantity){
+	    value=quantity;
+	}
+	
+	public LNode(int quantity, LNode nextOne){
+	    value=quantity;
+	    next=nextOne;
+	}
+	
+        public String toString(){
+	    return value+" ";
+	}
     }
 
+    public MyLinkedList(){
+    }
+
+    public Iterator<Integer> iterator(){
+	return new MLLI(this);
+    }
+    
     public int size(){
 	return size;
     }
-
-    public void add(LNode node){
+    
+    private LNode getNthNode(int index){
+	if(index >= size || index < 0){
+	    throw new IndexOutOfBoundsException();
+	}else{
+	    LNode here=head;
+	    for(int x=0; x<index; x++){
+		here = here.next;
+	    }
+	    return here;
+	}
+    }
+    
+    private void addAfter(LNode location, LNode toBeAdded){
+	location.next.prev = toBeAdded;
+	toBeAdded.next = location.next;
+	location.next = toBeAdded;
+	toBeAdded.prev = location;
 	size++;
-	node.next = start;
-	start = node;
+    }
+    
+    private void remove(LNode target){
+	if(target == head){
+	    head = target.next;
+	}else if(target == tail){
+	    tail = target.prev;
+	}else{
+	    target.prev.next = target.next;
+	    target.next.prev = target.prev;
+	}
     }
 
+    public int remove(int index){
+	LNode here = getNthNode(index);
+	int old = here.value;
+	remove(here);
+	size--;
+	return old;
+    }
+    
     public String toString(){
-	String str="";
-	LNode here = start;
-	for(int x=0; x<this.size(); x++){
-	    str+=here.value();
-	    here=here.next();
+	LNode here=head;
+	String newstr="[";
+	for(int x=0; x<size; x++){
+	    if(x==0){
+		newstr+=here.value+"("+here.next.value+"),";
+	    }else{
+		if(x==(size-1)){
+		    newstr+="("+here.prev.value+")"+here.value+"]";
+		}else{
+		    newstr+="("+here.prev.value+")"+here.value+"("+here.next.value+"),";
+		}
+	    }
+	    here=here.next;
 	}
-	return str;
+	return newstr;       
     }
 
-    public class LNode{
-	private int value;
-	private LNode next;
-
-	public LNode(int quantity, LNode nextNode){
-	    value=quantity;
-	    nextNode=null;
+    public int get(int index){
+        return getNthNode(index).value;
+    }
+    
+    public int set(int index, int value){
+	LNode here = getNthNode(index);
+	int old = here.value;
+	here.value = value;
+	return old;
+    }
+    
+    public int indexOf(int value){
+	int index=0;
+	LNode here=head;
+	for(int x=0; x<size; x++){
+	    if(here.value==value){
+		return index;
+	    }
+	    index++;
 	}
-	
-	public int value(){
-	    return value;
-	}
-	
-	public LNode next(){
-	    return next;
-	}
+	return -1;
     }
 
+    public boolean add(int value){
+	LNode addition = new LNode(value);
+	if(size == 0){
+	    head = addition;
+	}else if(size == 1){
+	    head.next = addition;
+	    addition.prev = head;
+	    tail = addition;
+	}else{
+	    tail.next = addition;
+	    addition.prev = tail;
+	    tail = addition;
+	}
+	size++;
+	return true;
+    }
+    
+    public void add(int index, int value){
+	LNode addition = new LNode(value);
+	LNode location = getNthNode(index-1);
+	addAfter(location,addition);	
+	
+	
+    }
     public static void main(String[]args){
-	MyLinkedList test = new MyLinkedList();
-	MyLinkedList.LNode e = test.new LNode(5,null);
-	MyLinkedList.LNode d = test.new LNode(4,null);
-	MyLinkedList.LNode c = test.new LNode(3,null);
-	MyLinkedList.LNode b = test.new LNode(2,null);
-	MyLinkedList.LNode a = test.new LNode(1,null);
+	MyLinkedList test=new MyLinkedList();
+	for(int x=0; x<10; x++){
+	    test.add(x);
+	}
+	System.out.println(test);
+	// test.remove(7);
+	// System.out.println(test);
+	// test.add(7,100);
+	// System.out.println(test);
+	// test.add(1234556789);
+	// System.out.println(test);
+	// test.set(3,987654321);
+	// System.out.println(test);
 
-	test.add(e);
-	test.add(d);
-	test.add(c);
-	test.add(b);
-	test.add(a);
-
-	System.out.println(test.toString());
-
-	
-	
-        
+	Iterator a = test.iterator();
+	while(a.hasNext()){
+	    System.out.println(a.next());
+	}
     }
 
+    public class MLLI implements Iterator<Integer>{
+	private MyLinkedList list;
+	private int x;
+
+	public MLLI(MyLinkedList l){
+	    list = l;
+	    x = 0;
+	}
+
+	public boolean hasNext(){
+	    return list.size > x;
+	}
+
+	public Integer next(){
+	    if(hasNext()){
+		x++;
+		return list.get(x-1);
+	    }else{
+		throw new NoSuchElementException();
+	    }
+	}
+
+	public void remove(){
+	    throw new UnsupportedOperationException();
+	}
+    }
 }
     
